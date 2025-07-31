baedd5efa0a951acf321aebe3c003798bd4db093
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-07-31 19:19:47 +0100

    simplify code logic and fix logging issue

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ impl DurableObject for FileShare {
                 self.set_data(&mut req).await?;
                 self.set_ttl(STORAGE_DURATION).await?;
                 self.set_alarm().await?;
+                self.set_key(&mut req).await?;
             }
             "/delete" => {
                 self.state.storage().delete_all().await?;
@@ -52,7 +53,15 @@ impl DurableObject for FileShare {
     async fn alarm(&self) -> Result<Response> {
         console_log!(
             "Deleting item for id: {}",
-            self.state.id().name().unwrap_or("null".to_string())
+            match self.state.id().name() {
+                Some(v) => v,
+                None => self
+                    .state
+                    .storage()
+                    .get("key")
+                    .await
+                    .unwrap_or("null".to_string()),
+            }
         );
         self.state.storage().delete_all().await?;
         Response::empty()
@@ -66,6 +75,14 @@ impl FileShare {
             .storage()
             .put("expire_at", js_sys::Date::now() / 1000.0 + (ttl as f64))
             .await?;
+        Ok(())
+    }
+
+    async fn set_key(&self, req: &mut Request) -> Result<()> {
+        match req.headers().get("key") {
+            Ok(Some(v)) => self.state.storage().put("key", v).await?,
+            _ => {}
+        };
         Ok(())
     }
 
@@ -98,52 +115,50 @@ impl FileShare {
     }
 }
 
+#[inline]
 fn get_rand() -> u32 {
     ((js_sys::Math::random() * (1e9 - 1e8 as f64)) as u32) + (1e8 as u32)
 }
 
-struct ClientInfo {
-    pub ip: String,
-    pub ray_id: String,
-    pub user_agent: String,
+fn strip(string: &String, part: Vec<&str>) -> String {
+    let mut out_string: String = string.clone();
+    for v in part.iter() {
+        out_string = match out_string.strip_prefix(v) {
+            Some(stripped) => String::from(stripped),
+            None => out_string,
+        };
+    }
+    out_string
 }
 
-fn get_info(req: &Request) -> ClientInfo {
-    let ip: String = req
-        .headers()
-        .get(&"cf-connecting-ip")
-        .unwrap_or(Some("null".to_string()))
-        .unwrap_or("null".to_string());
-    let ray_id: String = req
-        .headers()
-        .get(&"cf-ray")
-        .unwrap_or(Some("null".to_string()))
-        .unwrap_or("null".to_string());
-    let user_agent: String = req
-        .headers()
-        .get(&"user-agent")
-        .unwrap_or(Some("null".to_string()))
-        .unwrap_or("null".to_string());
-    ClientInfo {
-        ip: ip,
-        ray_id: ray_id,
-        user_agent: user_agent,
+#[inline]
+fn get_fully_qualified(instance_id: &String, file_id: &String) -> String {
+    format!("{}:::{}", instance_id, file_id)
+}
+
+#[inline]
+fn create_id(instance_id: &String) -> (String, String) {
+    let file_id = get_rand().to_string();
+    let out = get_fully_qualified(&instance_id, &file_id);
+    (file_id, out)
+}
+
+async fn check_active(stub: &Stub) -> Result<i64> {
+    match stub
+        .fetch_with_str("https://worker/is_active")
+        .await?
+        .text()
+        .await?
+        .as_str()
+    {
+        "false" => Ok(-1),
+        other => Ok(other.parse::<i64>().unwrap()),
     }
 }
 
 #[event(fetch)]
 async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     console_error_panic_hook::set_once();
-    // let client_info: ClientInfo = get_info(&req);
-    // console_log!(
-    //     "{} - [{}] : {} {}\nUser Agent: {}",
-    //     client_info.ip,
-    //     client_info.ray_id,
-    //     req.method().to_string(),
-    //     req.url()?.path().to_string(),
-    //     client_info.user_agent
-    // );
-    // let router = Router::with_data(client_info);
     let router = Router::new();
 
     router
@@ -151,40 +166,24 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
             let content = req.bytes().await.unwrap_or(vec![]);
             let namespace = ctx.durable_object("file_share")?;
             let mut instance_id = String::from(req.url()?.path().to_owned());
-            instance_id = match instance_id.strip_prefix("/upload") {
-                Some(v) => String::from(v),
-                None => instance_id,
-            };
-            instance_id = match instance_id.strip_prefix("/") {
-                Some(v) => String::from(v),
-                None => instance_id,
-            };
-            let mut file_id = get_rand().to_string(); // always 8 digit
-            let mut fully_qualified_id = format!("{}:::{}", instance_id, file_id);
-            let mut item = namespace.id_from_name(&fully_qualified_id.as_str())?;
-            let mut stub = item.get_stub()?;
-            let mut result = stub
-                .fetch_with_str("https://worker/is_active")
-                .await?
-                .text()
-                .await?;
-            while result != "false".to_string() {
+            instance_id = strip(&instance_id, vec!["/upload", "/"]);
+            let (mut file_id, mut fully_qualified_id) = create_id(&instance_id);
+            let mut stub = namespace.id_from_name(&fully_qualified_id.as_str())?.get_stub()?;
+            let mut result = check_active(&stub).await?;
+            while result != -1 {
                 console_warn!("Attempt to use id: {} but already used by another item, with {}ms remaining before expire", fully_qualified_id, result);
-                file_id = get_rand().to_string(); // always 8 digit
-                fully_qualified_id = format!("{}:::{}", instance_id, file_id);
-                item = namespace.id_from_name(&fully_qualified_id.as_str())?;
-                stub = item.get_stub()?;
-                result = stub
-                    .fetch_with_str("https://worker/is_active")
-                    .await?
-                    .text()
-                    .await?;
+                (file_id,  fully_qualified_id) = create_id(&instance_id);
+                stub = namespace.id_from_name(&fully_qualified_id.as_str())?.get_stub()?;
+                result = check_active(&stub).await?;
             }
+            let header: Headers = Headers::new();
+            header.append("key", fully_qualified_id.as_str())?;
             stub.fetch_with_request(Request::new_with_init(
                 "https://worker/set_data",
                 RequestInit::new()
                     .with_body(Some(js_sys::Uint8Array::from(content.as_slice()).into()))
-                    .with_method(Method::Post),
+                    .with_method(Method::Post)
+                    .with_headers(header),
             )?)
             .await?;
             console_log!(
@@ -193,20 +192,12 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
                 content.len(),
             );
             console_log!("Data: {}", String::from_utf8_lossy(content.as_slice()));
-            // stub.fetch_with_request(Request::new("set_data", Method::Post)?).await.unwrap();
             Response::ok(file_id)
         })
         .get_async("/download/:fully_qualified", |req, ctx| async move {
             let namespace = ctx.durable_object("file_share")?;
             let mut fully_qualified_id = String::from(req.url()?.path().to_owned());
-            fully_qualified_id = match fully_qualified_id.strip_prefix("/download") {
-                Some(v) => String::from(v),
-                None => fully_qualified_id,
-            };
-            fully_qualified_id = match fully_qualified_id.strip_prefix("/") {
-                Some(v) => String::from(v),
-                None => fully_qualified_id,
-            };
+            fully_qualified_id = strip(&instance_id, vec!["/download", "/"]);
             let item = namespace.id_from_name(&fully_qualified_id)?;
             let stub = item.get_stub()?;
             console_log!(
